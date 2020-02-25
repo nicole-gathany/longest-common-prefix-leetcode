f008db2ee0a4f2a2d48a29e3ec1101b2af0e96f3
Nicole Gathany <nicole.gathany@gmail.com>
2020-02-25 11:16:24 -0600

    more notes
    notes on the syntax i want to use

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
  */
 var longestCommonPrefix = function(strs) {
   // i want something that looks at all substrings in every word
+  //start with a for loop and then test
   // let test = strs.every(x => x)
+  //syntax: string.substr(start, length), the start will always be 0, the length may change
 };
 
 longestCommonPrefix(["flower", "flow", "flight"]);
