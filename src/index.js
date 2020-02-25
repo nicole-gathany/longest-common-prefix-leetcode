1c8f366170367dfd7dc53b6113fbbaf0e67295c1
Nicole Gathany <nicole.gathany@gmail.com>
2020-02-25 11:40:22 -0600

    console isn't working
    i'm trying to get my console to work. can't figure out what's wrong.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  // i have a feeling I would have to use substrings and loops
+  // i want something that looks at all substrings in every word
+  //start with a for loop and then test
+  // let test = strs.every(x => x)
+  //syntax: string.substr(start, length), the start will always be 0, the length may change
+  for (let i = 0; i < strs.length; i++) {
+    console.log(strs[i]);
+  }
+
+  console.log("Hi");
 };
 
 longestCommonPrefix(["flower", "flow", "flight"]);
