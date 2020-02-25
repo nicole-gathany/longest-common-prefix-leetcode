0c052666e22bd79587bed501b7c7d208887841ac
Nicole Gathany <nicole.gathany@gmail.com>
2020-02-25 11:18:51 -0600

    added an idea
    ...but the console is not working

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var longestCommonPrefix = function(strs) {
   //start with a for loop and then test
   // let test = strs.every(x => x)
   //syntax: string.substr(start, length), the start will always be 0, the length may change
+  for (let i = 0; i < strs.length; i++) {
+    console.log(strs[i]);
+  }
+
+  console.log("Hi");
 };
 
 longestCommonPrefix(["flower", "flow", "flight"]);
